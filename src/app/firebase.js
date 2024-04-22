a6118fa2888286cec18e2ea4b37204990a219703
Caleb Santes <caleb.mosq55@gmail.com>
2024-04-22 16:20:59 -0600

    V1.1.7 Se agrego parte de la autenticacion de usuario por vi email solo funcionando en SignUp y aun no acoplado a SignIn aun falta mucho para que la autenticacion funcione *Nota:Encontrar por qu elos datos que aparecen en consola no se les agrega el metodo Auth *Nota 2: Debe estar funcionando antes del 01/04/2024

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js"// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -15,4 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-console.log(app)
+export const auth= getAuth(app)
--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -1,6 +1,20 @@
-const signupForm = document.querySelector('.modal-contenido')
-signupForm.addEventListener('submit',(e) =>{
- const email=signupForm['signup-email']
- const password=signupForm['signup-password']
- console.log(email,password)
-})
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { auth } from './firebase.js';
+
+const signupForm = document.querySelector('.modal-contenido');
+signupForm.addEventListener('submit', async (e) => { // Marcar la función como async
+    e.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+
+    const email = signupForm['.emailInput2'].value; // Obtener el valor del campo de email
+    const password = signupForm['.passwordInput2'].value; // Obtener el valor del campo de contraseña
+
+    console.log(email, password);
+
+    // Sign up with email
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(userCredentials);
+    } catch (error) {
+        console.log(error);
+    }
+});
